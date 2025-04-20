9f333909a77a8f18aa41492b4eb82ef6b56e0899
jake <mubok0212@gmail.com>
2025-04-20 22:43:05 +0900

    modify prompt for LLM

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -13,11 +13,15 @@ export async function POST(request: Request) {
         { status: 400 }
       );
     }
-    console.log("messages history", messages);
-   
+    console.log("messages #1", messages);
+    messages[0].content = `
+      ${messages[0].content} 팩트체크 해줘.
+      답변은 .md 파일로 저장할 수 있게 마크다운 형식으로 제공해줘. 
+      답변은 최대한 상세하게 제공해줘.
+    `
+    console.log("messages #2", messages);
     const instructions = `
-    답변은 .md 파일로 저장할 수 있게 마크다운 형식으로 제공해줘. 
-    팩트 체크 외의 요청은 "수행할 수 없습니다"고 답변해줘.
+      팩트 체크 외의 요청은 "수행할 수 없습니다"고 답변해줘.
     `
     const response = await client.responses.create({
       model: "gpt-4.1",
@@ -26,7 +30,7 @@ export async function POST(request: Request) {
       tools: [ { type: "web_search_preview" } ],
     });
     console.log("response", response);
-    // console.log("content", response.output[1].content[0].annotations);
+
     return NextResponse.json(response);
   } catch (error) {
     console.error('Error in chat API:', error);
